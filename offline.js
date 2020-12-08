8be5f8e83e1f7c4e40c4a9de12c12bff4986fe54
KokokAyam <75647574+KokokAyam@users.noreply.github.com>
2020-12-09 05:55:52 +0700

    initial commit

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1607467791,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/pipe1-sheet0.png",
+		"images/pipe2-sheet0.png",
+		"images/pipe4-sheet0.png",
+		"images/pipe4-sheet1.png",
+		"images/pipe5-sheet0.png",
+		"images/pipe6-sheet0.png",
+		"images/pipe6-sheet1.png",
+		"images/pipe10-sheet0.png",
+		"images/pipe10-sheet1.png",
+		"images/pump-sheet0.png",
+		"images/pump-sheet1.png",
+		"images/pipebottom-sheet0.png",
+		"images/pipebottom-sheet1.png",
+		"images/stir-sheet0.png",
+		"images/stir-sheet1.png",
+		"images/water-sheet0.png",
+		"images/winloses-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
